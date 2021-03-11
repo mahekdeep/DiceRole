ec14c6922474725bf2fcbdd8e92e0ca4f88cd0d4
mahekdeep <mahekdeep1996@gmail.com>
2021-03-11 23:21:05 +0000

    Added if loops to ensure only acceptable values are y/n. Everything other input value will result in "error message.

--- main.py
+++ main.py
@@ -24,4 +24,13 @@ while roll == "y":
   print("The values are ", dice1,", ",dice2)
   #print(dice1,dice2)
   
+  #Asking if the user wants to role the dice again. 
+
   roll_again = input("Roll It again?: ")
+  if roll_again == 'n':
+    break
+  
+  if roll_again != 'n' and roll_again != 'y' :
+      print("Error: Not Acceptable Input Value (y/n). Please try run the code again.")
+      break
+
